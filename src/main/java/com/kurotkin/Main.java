9505ecc672ae17be3c2975e5431d6cb7b8de8288
kurotkin <kurotkin@users.noreply.github.com>
2018-01-18 16:47:23 +0300

    added main class

---
+++ src/main/java/com/kurotkin/Main.java
@@ -0,0 +1,15 @@
+package com.kurotkin;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+public class Main {
+    public static void main(String[] args) {
+        java.util.logging.Logger.getLogger("org.mongodb.driver").setLevel(Level.SEVERE);
+
+
+
+    }
+}
