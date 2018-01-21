75b78e455068df4d30d06d31c7719afd5364481a
kurotkin <kurotkin@users.noreply.github.com>
2018-01-21 23:50:11 +0300

    in find

--- src/main/java/com/kurotkin/Main.java
+++ src/main/java/com/kurotkin/Main.java
@@ -16,65 +16,82 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) throws UnirestException, InterruptedException {
 
-        Arduino arduino = new Arduino("COM3", 9600);
+        Arduino arduino = new Arduino("/dev/cu.usbmodem1421", 9600);
 
         while (true){
             boolean connected = arduino.openConnection();
             if(connected) System.out.print("connected: ");
-
-            HttpResponse<JsonNode>  f = Unirest.get("https://api.nicehash.com/api")
-                    .queryString("method", "stats.provider.ex")
-                    .queryString("addr", "3MocyP1djGcdvyg693nMhsQtNo3AL7Uve1")
-                    .asJson();
-            HttpResponse<JsonNode>  coinHttpResponse = Unirest.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/")
-                    .queryString("convert", "RUB")
-                    .asJson();
+            Thread.sleep(1000);
+            arduino.serialWrite("0.0 0.0 0.0 0\n");
 
             double profitability = 0.0;
             double balance = 0.0;
             double speed = 0.0;
             int algo = 0;
 
-            goBack:
-            try {
-                ResponseProvider result = new Gson().fromJson(f.getBody().toString(), ResponseProvider.class);
-                for(int i = 0; i < result.result.current.size(); i ++){
-                    profitability += result.result.current.get(i).profitability;
+            goBack: {
+                HttpResponse<JsonNode> f = Unirest.get("https://api.nicehash.com/api")
+                        .queryString("method", "stats.provider.ex")
+                        .queryString("addr", "3MocyP1djGcdvyg693nMhsQtNo3AL7Uve1")
+                        .asJson();
 
-                    String s = result.result.current.get(i).data.toString();
-                    s = s.substring(1, s.length() - 1);
-                    String aS[] = s.split(", ");
-
-                    if(aS[0].length() > 2) {
-                        speed += Double.parseDouble(aS[0].substring(4, aS[0].length() - 1));
-                        algo = result.result.current.get(i).algo;
-                    }
-
-                    balance += Double.parseDouble(aS[1]);
-                }
-            } catch (Exception E){
                 try {
-                    ResponseProviderWithError result = new Gson().fromJson(f.getBody().toString(), ResponseProviderWithError.class);
-                    String aS[] = result.result.error.split(" ");
-                    int timeDelay = Integer.parseInt(aS[13]);
-                    System.out.println("Delay " + timeDelay + " sec");
-                    Thread.sleep(timeDelay * 1000);
-                    break goBack;
-                } catch (Exception Er){
-                    System.err.println(f.getBody().toString());
-                    E.printStackTrace();
-                    Er.printStackTrace();
+                    ResponseProvider result = new Gson().fromJson(f.getBody().toString(), ResponseProvider.class);
+                    for (int i = 0; i < result.result.current.size(); i++) {
+                        profitability += result.result.current.get(i).profitability;
+
+                        String s = result.result.current.get(i).data.toString();
+                        s = s.substring(1, s.length() - 1);
+                        String aS[] = s.split(", ");
+
+                        if (aS[0].length() > 2) {
+                            try {
+                                speed += Double.parseDouble(aS[0].substring(4, aS[0].length() - 1));
+                            } catch (Exception E) {
+                                System.err.println("Error parsing speed");
+                            }
+
+                            algo = result.result.current.get(i).algo;
+                        }
+
+                        try {
+                            balance += Double.parseDouble(aS[1]);
+                        } catch (Exception E) {
+                            System.err.println("Error parsing balance");
+                        }
+                    }
+                } catch (Exception E) {
+                    try {
+                        ResponseProviderWithError result = new Gson().fromJson(f.getBody().toString(), ResponseProviderWithError.class);
+                        String aS[] = result.result.error.split(" ");
+                        int timeDelay = Integer.parseInt(aS[13]);
+                        System.out.println("Delay " + timeDelay + " sec");
+                        Thread.sleep(timeDelay * 1000);
+                        break goBack;
+                    } catch (Exception Er) {
+                        System.err.println(f.getBody().toString());
+                        E.printStackTrace();
+                        Er.printStackTrace();
+                    }
                 }
+
             }
 
-
             double rb = 0.0;
+            HttpResponse<JsonNode> coinHttpResponse = Unirest.get("https://api.coinmarketcap.com/v1/ticker/bitcoin/")
+                        .queryString("convert", "RUB")
+                        .asJson();
             try {
                 String coinResultString = "{result:" + coinHttpResponse.getBody().toString() + "}";
                 ResponseBitcoinRub coinResult = new Gson().fromJson(coinResultString, ResponseBitcoinRub.class);
                 String price_rub = coinResult.result.get(0).price_rub;
-                rb = Double.parseDouble(price_rub);
-            } catch (Exception E){
+                try {
+                    rb = Double.parseDouble(price_rub);
+                } catch (Exception E) {
+                    System.err.println("Error parsing rb");
+                }
+
+            } catch (Exception E) {
                 E.printStackTrace();
             }
 
@@ -84,10 +101,10 @@ public class Main {
             String toArduino = "";
             toArduino += String.format(Locale.US, "%.2f", profitability) + " ";
             toArduino += String.format(Locale.US, "%.2f", balance) + " ";
-            toArduino += String.format(Locale.US, "%.2f",speed) + " ";
+            toArduino += String.format(Locale.US, "%.2f", speed) + " ";
             toArduino += algo + "\n";
-            arduino.serialWriteln(toArduino);
-            System.out.print(toArduino);
+            arduino.serialWrite(toArduino);
+            System.out.println(toArduino);
             arduino.closeConnection();
             Thread.sleep(60000);
         }
