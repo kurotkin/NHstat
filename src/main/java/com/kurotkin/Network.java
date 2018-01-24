66f333c3ad495c24fbe1fdb999fee5836a4c03f5
kurotkin <kurotkin@users.noreply.github.com>
2018-01-25 00:17:47 +0300

    added a writer to db

--- src/main/java/com/kurotkin/Network.java
+++ src/main/java/com/kurotkin/Network.java
@@ -1,64 +1,79 @@
 package com.kurotkin;
 
-import com.google.gson.Gson;
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.esotericsoftware.yamlbeans.YamlReader;
 import com.kurotkin.controller.Nicehash;
 import com.kurotkin.controller.Rate;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.BatchPoints;
+import org.influxdb.dto.Point;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.math.BigDecimal;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Network {
+
     private static String Nicehash;
     private static String InfluxDBUrl;
     private static String InfluxDBUser;
     private static String InfluxDBPass;
     private static String InfluxDBdbName;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        loadSetting();
 
         while (true){
             Long t1 = System.currentTimeMillis();
 
-            String HTTPgetterUrl = Settings.getHTTPgetterUrl();
-            HTTPgetter getJson = new HTTPgetter(HTTPgetterUrl);
-            String jsonLine = getJson.get();
+            Rate rate = new Rate();
+            Nicehash nicehash = new Nicehash(Nicehash, rate);
 
-            Gson gson = new Gson();
-            Device[] data = gson.fromJson(jsonLine, Device[].class);
 
-            Influxdb influxdb = new Influxdb();
-            influxdb.insert(data);
+            BigDecimal speed = nicehash.getSpeed();
+            BigDecimal price_usd;
+            BigDecimal percent_change_1h;
+            BigDecimal percent_change_24h;
+            BigDecimal percent_change_7d;
+            BigDecimal price_rub;
+
+            int algo = nicehash.getAlgo();
+            try {
+                InfluxDB influxDB = InfluxDBFactory.connect(InfluxDBUrl, InfluxDBUser, InfluxDBPass);
+                influxDB.createDatabase(InfluxDBdbName);
+                BatchPoints batchPoints = BatchPoints
+                        .database(InfluxDBdbName)
+                        //.tag("async", "true")
+                        .retentionPolicy("autogen")
+                        .consistency(InfluxDB.ConsistencyLevel.ALL)
+                        .build();
+
+                Point.Builder builder = Point.measurement("Bitcoin");
+                builder.time(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+                builder.addField("profitability", nicehash.getProfitability().doubleValue());
+                builder.addField("balance", nicehash.getBalance().doubleValue());
+                builder.addField("profitability", nicehash.getProfitability().doubleValue());
+                builder.addField("profitability", nicehash.getProfitability().doubleValue());
+
+                batchPoints.point(builder.build());
+                influxDB.write(batchPoints);
+            }
+            catch (Exception e){
+                System.out.println("Создание БД слишком долгое: " + e);
+            }
 
             Long t2 = System.currentTimeMillis();
             Long dt = t2 - t1;
-
             System.out.println("Запись... Время записи " + dt + "ms");
 
             try {
-                Thread.sleep(50000 - dt);
+                Thread.sleep(120000 - dt);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            catch (IllegalArgumentException e){
-                System.out.println("Очень долго: " + e);
-            }
-
-        }
-
-
-        while (true){
-            Rate rate = new Rate();
-            Nicehash nicehash = new Nicehash("3MocyP1djGcdvyg693nMhsQtNo3AL7Uve1", rate);
-
-            BigDecimal profitability = nicehash.getProfitability();
-            BigDecimal balance = nicehash.getBalance();
-            BigDecimal speed = nicehash.getSpeed();
-            int algo = nicehash.getAlgo();
-
-            String toArduino = createStringToSerial(profitability, balance, speed, algo);
-            writeBytes(toArduino);
-            System.out.println(toArduino);
-            Thread.sleep(60000);
         }
     }
 
