84b7aba55762a313184d90c5e2118a63008bdbae
kurotkin <kurotkin@users.noreply.github.com>
2018-01-29 15:38:39 +0300

    added classes for api

---
+++ src/main/java/com/kurotkin/api/com/nicehash/api/Balance.java
@@ -0,0 +1,6 @@
+package com.kurotkin.api.com.nicehash.api;
+
+public class Balance {
+    public BalanceResult result;
+    public String method;
+}
---
+++ src/main/java/com/kurotkin/api/com/nicehash/api/BalanceResult.java
@@ -0,0 +1,6 @@
+package com.kurotkin.api.com.nicehash.api;
+
+public class BalanceResult {
+    public String balance_pending;
+    public String balance_confirmed;
+}
